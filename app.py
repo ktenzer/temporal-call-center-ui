c240407a760ceb7ad797fce76abe7551c22383eb
ktenzer <keith.tenzer@gmail.com>
2023-10-11 11:16:14 -0700

    implemented http server for twilio API integration, colors for call status and sorting of call statuses
    Signed-off-by: ktenzer <keith.tenzer@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
+from twilio.twiml.voice_response import VoiceResponse
+from twilio.rest import Client
 from client import get_client
 from dataclasses import dataclass
+import os
+
+CALL_POOL_WORKFLOW="call-pool"
+ROUTING_METHOD = "routing-free-for-all"
 
 app = Flask(__name__)
-CALL_POOL_WORKFLOW="callpool-test"
+
+@dataclass
+class CallInput:
+    CallSid: str
+    From: str
+    To: str
+    Routing: str
 
 @dataclass
 class AddAgent:
@@ -14,10 +26,12 @@ class AddAgent:
 class RemoveAgent:
     name: str
 
+# Route for the dashboard
 @app.route("/")
 def home():
     return render_template("dashboard.html")
 
+# Route for querying calls and agents from call pool workflow
 @app.route("/data")
 async def data():
     client = await get_client()
@@ -34,6 +48,7 @@ async def data():
         'available_agents': available_agents
     })
 
+# Route to add a new agent to the call pool workflow
 @app.route('/add_agent', methods=['GET', 'POST'])
 async def add_agent():
     if request.method == 'POST':
@@ -41,7 +56,6 @@ async def add_agent():
         call_pool_workflow = client.get_workflow_handle(f'{CALL_POOL_WORKFLOW}')
         name = request.form.get('name')
         number = request.form.get('number')
-        # Add code to send remove signal
         
         # After adding the agent, redirect to the dashboard.
         add_agent = AddAgent(
@@ -53,6 +67,7 @@ async def add_agent():
     
     return render_template('add_agent.html')
 
+# Route to remove agent from call pool workflow
 @app.route("/remove_agent")
 async def remove_agent():
     # Extract agent info from query parameters
@@ -68,7 +83,7 @@ async def remove_agent():
     # Get available agents via query to pool workflow
     available_agents = await call_pool_workflow.query("available_agents")
 
-    # Find and remove the agent from available_agents (if exists)
+    # Find and remove the agent from available_agents
     available_agents = [agent for agent in available_agents if agent['name'] != name and agent['number'] != number]
 
     remove_agent = RemoveAgent(
@@ -82,6 +97,51 @@ async def remove_agent():
         'calls': calls,
         'available_agents': available_agents
     })
+
+# Route for updating state of call in the workflow
+@app.route("/voice-status", methods=['POST'])
+async def voice_status():
+    call_sid=request.form['CallSid']
+    call_status=request.form['CallStatus']
+
+    try:
+        client = await get_client()
+        call_workflow = client.get_workflow_handle(call_sid)
+        await call_workflow.signal("updateCallStatus", call_status)
+    except:
+        print("Soft failure")
+
+    return jsonify({'status': 'ok',})
+
+# Route for providing twilio webhook to start call workflow for managing call
+@app.route("/voice", methods=['POST'])
+async def voice():
+    call_to=request.form['From']
+    call_from=request.form['To']
+    call_sid=request.form['CallSid']
+
+    response = VoiceResponse()
+    response.say('Thank you for calling Temporal Support.');
+    response.say('Please wait while I connect you to the next available agent.')
+    response.play('https://flaky-account-8453.twil.io/assets/sample.mp3')
+
+    input = CallInput(
+        CallSid=call_sid,
+        From=call_from,
+        To=call_to,
+        Routing=ROUTING_METHOD
+    ) 
+
+    client = await get_client()
+
+    await client.start_workflow(
+        "taskWorkflow",
+        input,
+        id=call_sid,
+        task_queue=os.getenv("TEMPORAL_TASK_QUEUE"),
+    )
+
+    return str(response)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,5 +1,16 @@
 // Function to update tables with fresh data
 function updateTables(data) {
+    
+    const getStatusPriority = (status) => {
+        // Define the order/priority for each status
+        const statusPriority = {
+            'pending': 1,
+            'active': 2,
+            'completed': 3
+        };
+        return statusPriority[status.toLowerCase()] || 99; // Default priority
+    };
+
     const updateTable = (tableId, columns, data, actions = []) => {
         const table = document.getElementById(tableId);
         let tbody = table.querySelector('tbody');
@@ -8,9 +19,31 @@ function updateTables(data) {
             table.appendChild(tbody);
         }
         tbody.innerHTML = '';
-        
+
+        // Sort data based on status priority for 'calls' table
+        if (tableId === 'calls') {
+            data.sort((a, b) => getStatusPriority(a.status) - getStatusPriority(b.status));
+        }
+
         data.forEach(row => {
             const tr = document.createElement('tr');
+
+            if (tableId === 'calls') {
+                switch (row.status.toLowerCase()) {
+                    case 'active':
+                        tr.className = 'status-active';
+                        break;
+                    case 'pending':
+                        tr.className = 'status-pending';
+                        break;
+                    case 'completed':
+                        tr.className = 'status-completed';
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             columns.forEach(col => {
                 const td = document.createElement('td');
                 td.textContent = row[col.toLowerCase()];
@@ -50,7 +83,28 @@ function fetchData() {
         .then(data => updateTables(data))
         .catch(error => console.error('Error fetching data:', error));
     
-    setTimeout(fetchData, 5000);
+    setTimeout(fetchData, 1000);
+}
+
+function renderCallsTable(calls) {
+    const table = document.getElementById('calls');
+    table.innerHTML = `<tr>
+                            <th>Status</th>
+                            <th>Sid</th>
+                            <th>Caller</th>
+                            <th>Agent</th>
+                       </tr>`;
+    calls.forEach(call => {
+        const row = document.createElement('tr');
+        // Assign class based on status
+        row.className = `status-${call.status.toLowerCase()}`;
+
+        row.innerHTML = `<td>${call.status}</td>
+                         <td>${call.sid}</td>
+                         <td>${call.caller}</td>
+                         <td>${call.agent}</td>`;
+        table.appendChild(row);
+    });
 }
 
 // Start fetching data
